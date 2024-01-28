8d6c27b0fe3797a780315b31d01765bda44778bd
souvikadhikary <souvikadhikary238@gmail.com>
2024-01-29 00:19:37 +0530

    Complete useEffect

--- 37-advance-React/src/components/PostList.jsx
+++ 37-advance-React/src/components/PostList.jsx
@@ -1,29 +1,36 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostListContext } from "../stores/Post-List-Store";
 import WelComeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListContext);
   // console.log(postList);
+
+  const [fetching, setFetching] = useState(false);
+
   useEffect(() => {
-    fetch("https://dummyjson.com/posts")
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setFetching(true);
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
         addInitialPost(data.posts);
+        setFetching(false);
       });
+    return () => {
+      controller.abort();
+    };
   }, []);
-
-  const handleGetRequest = () => {};
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelComeMessage getRequest={handleGetRequest} />
-      )}
-      {postList.map((posts) => (
-        <Post key={posts.id} posts={posts} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {fetching && postList.length === 0 && <WelComeMessage />}
+      {!fetching &&
+        postList.map((posts) => <Post key={posts.id} posts={posts} />)}
     </>
   );
 };
--- 37-advance-React/src/components/WelcomeMessage.jsx
+++ 37-advance-React/src/components/WelcomeMessage.jsx
@@ -3,9 +3,6 @@ const WelComeMessage = ({ getRequest }) => {
     <>
       <center>
         <h1 className="welcome-message">There are 0 post you entered</h1>
-        <button onClick={getRequest} type="button" className="btn btn-primary">
-          Get Post From Server
-        </button>
       </center>
     </>
   );
